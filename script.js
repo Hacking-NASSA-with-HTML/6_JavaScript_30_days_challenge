881591fe769056e2632f658b28ca390dd115bd96
Hacking-NASSA-with-HTML <follow.trend7777@gmail.com>
2021-01-10 20:27:11 +0400

    feat: add let variables as a functions' name

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ fetch(endpoint)
     .then(blob => blob.json())
     .then(data => cities.push(...data));
 
-function findMatches(wordToMatch, cities) {
+let findMatches = function (wordToMatch, cities) {
     return cities.filter(place => {
         // here we need to figure out if the city or state matches what was searched
         const regex = new RegExp(wordToMatch, 'gi');
@@ -20,7 +20,24 @@ function findMatches(wordToMatch, cities) {
 const numberWithCommas = (x) => x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 
 
-function displayMatches() {
+// With vanilaJS
+// function displayMatches() {
+//     const matchArray = findMatches(this.value, cities);
+//     const html = matchArray.map(place => {
+//         const regex = new RegExp(this.value, 'gi');
+//         const cityName = place.city.replace(regex, `<span class="hl">${this.value}</span>`);
+//         const stateName = place.state.replace(regex, `<span class="hl">${this.value}</span>`);
+//         return `
+//             <li>
+//             <span class="name">${cityName}, ${stateName}</span>
+//             <span class="population">${numberWithCommas(place.population)}</span>
+//             </li>
+//         `;
+//     }).join('');
+//     suggestions.innerHTML = html;
+// }
+
+let displayMatches = function () {
     const matchArray = findMatches(this.value, cities);
     const html = matchArray.map(place => {
         const regex = new RegExp(this.value, 'gi');
