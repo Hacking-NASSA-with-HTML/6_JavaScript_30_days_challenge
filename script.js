88919801a42ec80303290c5f9d44268b0db56110
Hacking-NASSA-with-HTML <follow.trend7777@gmail.com>
2021-01-11 20:25:20 +0400

    feat: add "to console" and "to front-end" devider line 44-45

--- script.js
+++ script.js
@@ -39,20 +39,23 @@ const numberWithCommas = (x) => x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',
 
 let displayMatches = function () {
     const matchArray = findMatches(this.value, cities);
-    // This displaying all fetched data to console:
-    console.log(matchArray);
-    // const html = matchArray.map(place => {
-    //     const regex = new RegExp(this.value, 'gi');
-    //     const cityName = place.city.replace(regex, `<span class="hl">${this.value}</span>`);
-    //     const stateName = place.state.replace(regex, `<span class="hl">${this.value}</span>`);
-    //     return `
-    //         <li>
-    //         <span class="name">${cityName}, ${stateName}</span>
-    //         <span class="population">${numberWithCommas(place.population)}</span>
-    //         </li>
-    //     `;
-    // }).join('');
-    // suggestions.innerHTML = html;
+    // This code displaying all fetched data to console:
+    //console.log(matchArray);
+
+
+    // This code displaying all fetched data to HTML:
+    const html = matchArray.map(place => {
+        const regex = new RegExp(this.value, 'gi');
+        const cityName = place.city.replace(regex, `<span class="hl">${this.value}</span>`);
+        const stateName = place.state.replace(regex, `<span class="hl">${this.value}</span>`);
+        return `
+            <li>
+            <span class="name">${cityName}, ${stateName}</span>
+            <span class="population">${numberWithCommas(place.population)}</span>
+            </li>
+        `;
+    }).join('');
+    suggestions.innerHTML = html;
 }
 
 const searchInput = document.querySelector('.search');
